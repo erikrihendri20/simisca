6bf95885e31399992b04ab3feeb59347d4610774
yudistiraeltonjhon <yudistiraelton@gmail.com>
2021-06-20 21:42:17 +0700

    monitoringberhenti

--- simisca/app/Views/dashboard/monitoringBerhenti.php
+++ simisca/app/Views/dashboard/monitoringBerhenti.php
@@ -1,6 +1,13 @@
 <?= $this->extend('template/dashboard/index'); ?>
 <?= $this->section('content'); ?>
-            <div class="menu-content">
-                Tidak sedang dalam masa survei
+<div class="menu-content">
+    <div id="notfound">
+        <div class="notfound">
+            <div class="icon">
+                <h1><i class="fas fa-bomb" data-aos="fade-down"></i></h1>
             </div>
+            <h2>Mohon maaf. Tidak sedang dalam masa survei.</h2>
+        </div>
+    </div>
+</div>
 <?= $this->endSection(); ?>
